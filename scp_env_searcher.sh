431cb4a1e18785db1b8f12f1b9f6117346177ee7
Popwers <lionel.bataille@hotmail.com>
2023-10-06 16:24:02 +0400

    Upgrade script and fix some miss

--- scp_env_searcher.sh
+++ scp_env_searcher.sh
@@ -12,6 +12,13 @@ WARNING="⚠️"
 INFO="ℹ️"
 ERROR="❌"
 
+# Initialize variables with default values
+SSH_KEY=""
+REMOTE_USER=""
+REMOTE_SERVER=""
+LOCAL_DIRECTORY=""
+FILE_PATTERN=""
+
 # Function to display progress messages
 show_progress() {
   local message_type="$1"
@@ -56,20 +63,26 @@ show_progress() {
 # Function to display usage instructions
 usage() {
   echo "Usage: $0 -i <SSH_KEY> -u <REMOTE_USER> -s <REMOTE_SERVER> -d <LOCAL_DIRECTORY> -f <FILE_PATTERN>"
+  echo "Options:"
+  echo "  -i  SSH_KEY        : SSH private key file (e.g., ./path/to/key.pem)"
+  echo "  -u  REMOTE_USER    : Remote user (e.g., admin)"
+  echo "  -s  REMOTE_SERVER  : Remote server address (e.g., ec2-xxx-xxx-xxx-xxx.compute.amazonaws.com)"
+  echo "  -d  LOCAL_DIRECTORY: Local directory where files will be copied"
+  echo "  -f  FILE_PATTERN   : File pattern to search for (e.g., '.env*')"
   exit 1
+}
+
+# Function to create the LOCAL_DIRECTORY if it doesn't exist
+create_local_directory() {
+  if [ ! -d "$1" ]; then
+    mkdir -p "$1"
+  fi
 }
 
 # Function to check if rsync is available on the remote server
 is_rsync_available() {
   ssh -i "$SSH_KEY" "$REMOTE_USER@$REMOTE_SERVER" "command -v rsync > /dev/null 2>&1 && echo 'true' || echo 'false'"
 }
-
-# Initialize variables with default values
-SSH_KEY=""
-REMOTE_USER=""
-REMOTE_SERVER=""
-LOCAL_DIRECTORY=""
-FILE_PATTERN=""
 
 # Parse command line arguments
 while getopts "i:u:s:d:f:" opt; do
@@ -96,7 +109,7 @@ while getopts "i:u:s:d:f:" opt; do
 done
 
 # Check if required parameters are provided
-if [ -z "$SSH_KEY" ] || [ -z "$REMOTE_USER" ] || [ -z "$REMOTE_SERVER" ] || [ -z "$LOCAL_DIRECTORY" ]; then
+if [ -z "$SSH_KEY" ] || [ -z "$REMOTE_USER" ] || [ -z "$REMOTE_SERVER" ] || [ -z "$LOCAL_DIRECTORY" ] || [ -z "$FILE_PATTERN" ]; then
   usage
 fi
 
@@ -113,20 +126,29 @@ SSH_OUTPUT=$(ssh -i "$SSH_KEY" "$REMOTE_USER@$REMOTE_SERVER" "$REMOTE_COMMAND")
 if [ -z "$SSH_OUTPUT" ]; then
   show_progress "info" "No $FILE_PATTERN files were found on $REMOTE_SERVER."
 else
+  # Check if the LOCAL_DIRECTORY exists or create it
+  create_local_directory "$LOCAL_DIRECTORY"
+
   # Display the found files
-  show_progress "info" "Found $FILE_PATTERN files on $REMOTE_SERVER :\n\n$SSH_OUTPUT"
+  show_progress "success" "Found $FILE_PATTERN files on $REMOTE_SERVER :\n\n$SSH_OUTPUT"
+  show_progress "" "" "\n"
 
   # Copy files using rsync if available, otherwise use scp
   if [ "$RSYNC_AVAILABLE" == "true" ]; then
     show_progress "info" "Copying files using rsync..."
-    rsync -avz -e "ssh -i $SSH_KEY" --progress --files-from=<(echo "$SSH_OUTPUT") "$REMOTE_USER@$REMOTE_SERVER:/" "$LOCAL_DIRECTORY"
+    show_progress "" "" "\n"
+    rsync -avz -e "ssh -i $SSH_KEY" --progress --relative --files-from=<(echo "$SSH_OUTPUT") "$REMOTE_USER@$REMOTE_SERVER:" "$LOCAL_DIRECTORY"
   else
     show_progress "info" "Copying files using scp..."
+    show_progress "" "" "\n"
     while IFS= read -r file; do
-      show_progress "info" "Copying $file to $LOCAL_DIRECTORY..."
-      scp -i "$SSH_KEY" "$REMOTE_USER@$REMOTE_SERVER:$file" "$LOCAL_DIRECTORY"
+      destination="$LOCAL_DIRECTORY/${file#*/}"
+      mkdir -p "$(dirname "$destination")"
+      show_progress "info" "Copying $file to $destination..."
+      scp -i "$SSH_KEY" "$REMOTE_USER@$REMOTE_SERVER:$file" "$destination"
     done <<< "$SSH_OUTPUT"
   fi
 
-  show_progress "success" "Copy completed. Files are copied to $LOCAL_DIRECTORY."
+  show_progress "" "" "\n"
+  show_progress "success" "\n\nCopy completed. Files are copied to $LOCAL_DIRECTORY."
 fi
